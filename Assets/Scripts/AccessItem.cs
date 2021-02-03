7ae8bc8ceae2e8218b62d5f89aa68f0189a41036
Mathijs van Dinther <ma.van.dinther@sintlucasedu.nl>
2021-02-03 15:07:04 +0100

    Items
    [Added]
    - Item (Name and Weight)
    - AccessItem (Item and DoorID)
    - BonusItem (Item and Points)

---
+++ Assets/Scripts/AccessItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessItem : Item
+{
+    private int doorId;
+
+    public AccessItem(string name, float weight, int door) : base(name, weight)
+    {
+
+    }
+
+    public int GetDoorId()
+    {
+        return doorId;
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    private string name;
+    private float weight;
+
+    public Item(string name, float weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public Item(string name)
+    {
+        this.name = name;
+        weight = 0;
+    }
+}
